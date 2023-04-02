607e548c6df8d8be02d9a5a949ce4f3c7d3af9d6
Rubenpe18 <ruben122aaa@gmail.com>
2023-04-02 22:41:21 +0200

    Primer Commit del TDD

---
+++ tests/TestCoche.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCoche {
+
+    @Test
+
+    public void test_al_crear_coche_su_velocidad_es_cero(){
+        Coche nuevoCoche=new Coche();
+        Assertions.assertEquals(0, nuevoCoche.velocidad);
+    }
+
+    @Test
+
+    public void test_al_acelerar_un_coche_su_velocidad_aumenta(){
+        Coche nuevoCoche=new Coche();
+        nuevoCoche.acelerar(30);
+        Assertions.assertEquals(30, nuevoCoche.velocidad);
+    }
+
+    @Test
+
+    public void test_al_decelerar_un_coche_su_velocidad_disminuye(){
+        Coche nuevoCoche=new Coche();
+        nuevoCoche.velocidad=50;
+        nuevoCoche.decelerar(20);
+        Assertions.assertEquals(30, nuevoCoche.velocidad);
+    }
+
+    @Test
+
+    public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero(){
+        Coche nuevoCoche=new Coche();
+        nuevoCoche.velocidad=50;
+        nuevoCoche.decelerar(80);
+        Assertions.assertEquals(0, nuevoCoche.velocidad);
+    }
+}
+class Coche{
+    public int velocidad;
+    public void acelerar(int aceleracion){
+        velocidad+=aceleracion;
+    }
+    public void decelerar(int decelarion){
+        velocidad -= decelarion;
+        if(velocidad<0)velocidad=0;
+    }
+}
